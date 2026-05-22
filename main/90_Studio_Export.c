76123e4f5bdd8466de8584e301f94a0185ca90fc
Scott Forster <papamoascotty@gmail.com>
2026-05-23 10:57:21 +1200

    FORGEUI_STUDIO_ONE_BUTTON_EXPORT_FLASH_PIPELINE_PROVEN

--- main/90_Studio_Export.c
+++ main/90_Studio_Export.c
@@ -7,8 +7,8 @@
 void fg_studio_export_create(lv_obj_t *parent)
 {
     lv_obj_t * obj1 = lv_label_create(parent);
-    lv_label_set_text(obj1, "Its a good day to code");
-    lv_obj_set_pos(obj1, 284, 121);
+    lv_label_set_text(obj1, "First flash from studio");
+    lv_obj_set_pos(obj1, 292, 122);
     lv_obj_set_style_text_color(obj1, lv_color_hex(0xFFFFFF), 0);
     lv_obj_set_style_text_font(obj1, &lv_font_montserrat_24, 0);
 
